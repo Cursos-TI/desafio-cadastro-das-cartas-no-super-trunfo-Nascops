f1dd956c8c8204f195ab0b1eb4ff7736dc0a86b2
Nascops <lucasndcbarbosa@gmail.com>
2025-02-13 12:04:12 +0000

    Registro de Cartas trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,17 +6,36 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+#include <string.h> // Biblioteca de manipulação de strings (strlen, strcpy, strcat, etc.)
+
+int main(){
+    char estado, codigo[4], nome[50];
+    int populacao, turismo;
+    float area;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Estado (Letra do Estado):\n");
+    scanf(" %c", &estado);  // Adiciona espaço antes do %c para ignorar espaços em branco e limita a um caractere
+
+    printf("Código (ex: A01, B02):\n");
+    scanf("%3s", codigo);  // Remove o & e limita a entrada e garante que lê até 3 caracteres
+
+    getchar(); // Limpa o buffer do teclado ante sdo scanf e faz funcionar o fgets
+
+    printf("Nome da Cidade:\n");
+    fgets(nome, sizeof(nome),stdin);
+    nome[strcspn(nome, "\n")] = '\0';  // Remove o '\n' do final da string
+
+    printf("População:\n");
+    scanf("%d", &populacao);
+
+    printf("Área:\n");
+    scanf("%f", &area);
+
+    printf("Número de Pontos Turísticos:\n");
+    scanf("%d", &turismo);
+
+    printf("\n=== Dados da Cidade ===\n");
+    printf("Estado: %c \nCódigo da Carta: %s \nNome da Cidade: %s \nPopulação: %d \nÁrea: %.2f km² \nNúmero de Pontos Turisticos: %d\n", estado, codigo, nome, populacao, area, turismo);
 
     return 0;
 }
